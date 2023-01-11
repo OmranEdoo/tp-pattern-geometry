ec5c9f11b3ce5874294ad705fc961ecf35201c2e
lfredoc <lea.fredoc@ensg.eu>
2023-01-11 15:20:08 +0100

    0.1 - Coordinate (2D)

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -5,14 +5,28 @@ public class Coordinate {
     private double y;
 
     public Coordinate(){
-
+        this.x = 0.0;
+        this.y = 0.0;
     };
 
     public Coordinate(double x, double y){
-
+        this.x = x;
+        this.y = y;
     };
 
-    public double getX(){
-        return this.x;
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
     }
 }
