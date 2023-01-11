c5ee7ed5dde764c2b587e4548f7e435de3f97aac
lfredoc <lea.fredoc@ensg.eu>
2023-01-11 15:14:55 +0100

    initialisation

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,18 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    private double x;
+    private double y;
 
+    public Coordinate(){
+
+    };
+
+    public Coordinate(double x, double y){
+
+    };
+
+    public double getX(){
+        return this.x;
+    }
 }
