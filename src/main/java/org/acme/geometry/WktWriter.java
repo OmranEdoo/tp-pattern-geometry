77a51d31aacc8256f4737602aa4cbc2b9e010f8b
OmranEdoo <omranedoo@gmail.com>
2023-01-12 15:57:34 +0100

    rectification pom.xml

--- src/main/java/org/acme/geometry/WktWriter.java
+++ src/main/java/org/acme/geometry/WktWriter.java
@@ -11,6 +11,9 @@ public class WktWriter {
     public String write(Geometry geometry){
         if ( geometry instanceof Point ){
             Point point = (Point)geometry;
+            if (point.isEmpty()){
+                return "POINT EMPTY";
+            }
             this.geomString = "POINT(" + String.valueOf(point.getCoordinate().getX()) + " " + String.valueOf(point.getCoordinate().getY()) + ")";
             return this.geomString;
         } else if ( geometry instanceof LineString ){
@@ -18,6 +21,9 @@ public class WktWriter {
             this.geomString = "LINESTRING(";
             int nbPoints = lineString.getNumPoints();
             for (int i = 0; i < nbPoints-1; i++){
+                if (lineString.isEmpty()){
+                    return "LINESTRING EMPTY";
+                }
                 this.geomString += String.valueOf(lineString.getPointN(i).getCoordinate().getX()) + " " + String.valueOf(lineString.getPointN(i).getCoordinate().getY()) + ",";
             }
             geomString += String.valueOf(lineString.getPointN(nbPoints-1).getCoordinate().getX()) + " " + String.valueOf(lineString.getPointN(nbPoints-1).getCoordinate().getY()) + ")";
--- src/test/java/org/acme/geometry/WktWriterTest.java
+++ src/test/java/org/acme/geometry/WktWriterTest.java
@@ -1,7 +1,10 @@
 package org.acme.geometry;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.internal.matchers.Null;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,31 @@ public class WktWriterTest {
         LineString lineString = new LineString(listePoints);
         Assert.assertEquals("POINT(4.0 2.0)", new WktWriter().write((Geometry) point1));
         Assert.assertEquals("LINESTRING(4.0 2.0,-4.0 -2.0)", new WktWriter().write((Geometry) lineString));
+    }
+
+    @Test
+    void testWriteEmpty() {
+        Coordinate coordinate1 = new Coordinate();
+        Point point1 = new Point(coordinate1);
+        Coordinate coordinate2 = new Coordinate();
+        Point point2 = new Point(coordinate2);
+        List<Point> listePoints = new ArrayList<Point>();
+        listePoints.add(point1);
+        listePoints.add(point2);
+        LineString lineString = new LineString(listePoints);
+        Assert.assertEquals("POINT EMPTY", new WktWriter().write((Geometry) point1));
+        Assert.assertEquals("LINESTRING EMPTY", new WktWriter().write((Geometry) lineString));
+    }
+
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void testWriteInvalidGeometry() {
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("geometry type not supported");
+        new WktWriter().write( null);
     }
 
 }
